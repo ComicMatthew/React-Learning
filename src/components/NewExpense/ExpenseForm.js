a0d93efa57dc6b2ed5929095c2aa1afe2c2cac4d
ComicMatthew <mateusz.a.staniek@gmail.com>
2023-01-25 20:32:18 +0100

    added SubmitHandler

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -15,7 +15,6 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-    console.log(event.target.value)
     // setUserInput({
     //   ...userInput,
     //   enteredTitle: event.target.value
